73a92820089b3e2f4a25d46c4cfbac4e0f1d0801
codewithjalloh <codewithjalloh@gmail.com>
2024-04-30 17:17:56 +0100

    app bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,28 @@
 import 'package:flutter/material.dart';
 
+import 'widgets/appbar.dart';
+
 void main() {
   runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          // App bar title
+          title: Text("Complete Widgets"),
+          // App bar background colour
+          backgroundColor: Colors.grey,
+          // controls the size of the shadow below the app bar
+          elevation: 50,
+        ),
+      ),
+    );
+  }
 }
