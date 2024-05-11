0e03a1e76ccac0d10906ed8327b81d320a417c17
codewithjalloh <codewithjalloh@gmail.com>
2024-05-11 09:08:18 +0100

    state management app done

--- lib/pages/counter_page_state_management.dart
+++ lib/pages/counter_page_state_management.dart
@@ -14,6 +14,31 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Consumer<CounterModel>(
-        builder: (context, value, child) => Scaffold());
+        builder: (context, value, child) => Scaffold(
+              appBar: AppBar(
+                backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+                title: Text(widget.title),
+              ),
+              body: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("Have you pushed the button this many times"),
+                    Text(
+                      value.count.toString(),
+                      style: Theme.of(context).textTheme.headlineMedium,
+                    ),
+                  ],
+                ),
+              ),
+              floatingActionButton: FloatingActionButton(
+                onPressed: () {
+                  final counter = context.read<CounterModel>();
+                  counter.increment();
+                },
+                tooltip: "Increment",
+                child: Icon(Icons.add),
+              ),
+            ));
   }
 }
